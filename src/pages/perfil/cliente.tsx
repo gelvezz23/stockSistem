47374ea68addd6045dcfcb54d0364c07d9e07a3f
gelvezz23 <mao_23_@hotmail.com>
2025-06-04 15:06:33 -0500

    [UPD] quite comentarios

--- src/pages/perfil/cliente.tsx
+++ src/pages/perfil/cliente.tsx
@@ -1,13 +1,20 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { useSessionStorage } from "../../utils/hook/useSessionStorage";
 import { FormsClient } from "../dashboard/usersList/form";
 import Modal from "../../components/Modal";
 import FormServicioTecnico from "../../components/FormServicioTecnico";
+import { IconoProceso } from "../../components/iconos/iconoProceso";
+import { IconoReagendar } from "../../components/iconos/iconoReagendar";
+import { DateTimer } from "../dashboard/citas/dateTimer";
 
 export const Cliente = () => {
   const { storage } = useSessionStorage("user", null);
   const [viewModalEdit, setViewModalEdit] = useState(false);
+  const [open3, setModal3] = useState(false);
+  const [minDate, setMinDate] = useState("");
+
   const [viewModalServicio, setViewModalServicio] = useState(false);
   const [clientsState, setClientState] = useState({
     nombre_cliente: "",
@@ -19,6 +26,45 @@ export const Cliente = () => {
   });
   const [errorData, setError] = useState<string | null>(null);
   const [loadingData, setLoading] = useState(true); // Initialize loading to true
+  const [citas, setCitas] = useState<any>([]);
+  const [selectedClient, setSelectedClient] = useState<any>();
+  const [_products, setProducts] = useState<any>();
+  const [selectedDate, setSelectedDate] = useState<any>();
+  const [selectedTime, setSelectedTime] = useState<any>();
+  const [showTime, setShowTime] = useState(false);
+
+  const handleShowSelectTimer = () => {
+    setShowTime(!showTime);
+  };
+  const fechaInicioFormateada = useCallback(() => {
+    return selectedDate ? `${selectedDate} ${selectedTime}:00` : undefined;
+  }, [selectedDate, selectedTime]);
+  const fecha = fechaInicioFormateada();
+
+  useEffect(() => {
+    const fetchCitas = async () => {
+      setLoading(true);
+      try {
+        const response = await fetch(
+          `${import.meta.env.VITE_BACK_URL}/api/getServicioTecnico`
+        );
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+        const newData = data.filter(
+          (item: any) => item.cliente_id === clientsState.usuario_id
+        );
+        setCitas(newData);
+      } catch (e: any) {
+        setError("Error al cargar las citas: " + e.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchCitas();
+  }, []);
 
   useEffect(() => {
     const getClient = async () => {
@@ -70,6 +116,25 @@ export const Cliente = () => {
     getClient();
   }, [storage?.user?.usuario_id]); // Use optional chaining in dependency array
 
+  useEffect(() => {
+    // Obtener la fecha actual en formato YYYY-MM-DD
+    const today = new Date();
+    const year = today.getFullYear();
+    const month = String(today.getMonth() + 1).padStart(2, "0"); // Meses son 0-indexados
+    const day = String(today.getDate()).padStart(2, "0");
+    setMinDate(`${year}-${month}-${day}`);
+  }, []);
+
+  const formatDate = (dateString: string): string => {
+    const date = new Date(dateString);
+    const day = date.getDate().toString().padStart(2, "0");
+    const month = (date.getMonth() + 1).toString().padStart(2, "0"); // getMonth() devuelve de 0 a 11
+    const year = date.getFullYear();
+    const hours = date.getHours().toString().padStart(2, "0");
+    const minutes = date.getMinutes().toString().padStart(2, "0");
+    return `${day}/${month}/${year} ${hours}:${minutes}`;
+  };
+
   if (loadingData) {
     return (
       <div className="bg-gray-100 min-h-screen flex items-center justify-center">
@@ -96,6 +161,57 @@ export const Cliente = () => {
     setViewModalServicio(!viewModalServicio);
   };
 
+  console.log({ citas, clientsState });
+
+  const compareFechaCitas = (cita: any) => {
+    const fechaCita = new Date(cita.fecha_inicio);
+    const fechaActual = new Date();
+    console.log("TT", fechaCita < fechaActual);
+    if (fechaCita < fechaActual) {
+      return true;
+    }
+    return false;
+  };
+
+  const handleOpenDataModal = (cita: any) => {
+    setSelectedClient(cita.servicio_id);
+    setProducts(cita.producto_id);
+  };
+  const handleSubmitEdit = async (e: any) => {
+    e.preventDefault();
+    const body = {
+      fecha_inicio: fecha,
+      cliente_id: selectedClient,
+    };
+
+    try {
+      const response = await fetch(
+        `${import.meta.env.VITE_BACK_URL}/api/citas/update`,
+        {
+          method: "PUT", // Utilizamos PUT para actualizar un recurso existente
+          headers: {
+            "Content-Type": "application/json", // Indicamos que el cuerpo de la solicitud es JSON
+          },
+          body: JSON.stringify(body), // Convertimos el objeto JavaScript 'body' a una cadena JSON
+        }
+      );
+
+      if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(
+          errorData.message || "Algo salió mal al actualizar el servicio."
+        );
+      }
+
+      const data = await response.json();
+      console.log("Servicio actualizado con éxito:", data);
+      alert("¡Servicio actualizado correctamente!");
+      window.location.reload();
+    } catch (error: any) {
+      console.error("Error al enviar la solicitud:", error);
+      alert(`Error al actualizar el servicio: ${error.message}`);
+    }
+  };
   return (
     <div className="bg-gray-100 min-h-screen py-8">
       <Modal isOpen={viewModalEdit} onClose={handleEditClickEdit}>
@@ -116,6 +232,13 @@ export const Cliente = () => {
             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
           >
             Editar
+          </button>
+
+          <button
+            onClick={handleEditClickEdit}
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          >
+            Agenda
           </button>
         </div>
         <div className="bg-white shadow overflow-hidden rounded-md">
@@ -162,6 +285,221 @@ export const Cliente = () => {
               {/* Puedes agregar más campos aquí si es necesario */}
             </dl>
           </div>
+        </div>
+
+        <div className="container mx-auto py-8 text-gray-900">
+          <h1 className="text-2xl font-semibold mb-4 text-gray-800">
+            Citas Agendadas
+          </h1>
+          {citas.length === 0 ? (
+            <p className="text-gray-600">No hay citas agendadas.</p>
+          ) : (
+            <div className="overflow-x-auto">
+              <table className="min-w-full leading-normal shadow-md rounded-lg">
+                <thead>
+                  <tr>
+                    <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
+                      ID
+                    </th>
+                    <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
+                      Fecha Inicio
+                    </th>
+
+                    <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
+                      Cliente
+                    </th>
+                    <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
+                      Técnico
+                    </th>
+                    <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
+                      Servicio
+                    </th>
+                    <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
+                      Estado
+                    </th>
+                    <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
+                      Dirección
+                    </th>
+                    <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
+                      Garantía
+                    </th>
+                    <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
+                      Descripción
+                    </th>
+                    <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
+                      Solución
+                    </th>
+                    <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
+                      Observación
+                    </th>
+                    <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
+                      Evidencia
+                    </th>
+
+                    <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
+                      .
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {citas.map((cita: any, index: any) => (
+                    <>
+                      <tr key={`${cita.servicio_id}_${index}`}>
+                        <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                          {`${formatDate(cita.fecha_inicio)
+                            .split(" ")[0]
+                            .replace(/\//g, "")}_${cita.servicio_id}`}
+                        </td>
+                        <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                          {formatDate(cita.fecha_inicio)}
+                        </td>
+
+                        <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                          {cita?.cliente_documento} - {cita.cliente_email}
+                        </td>
+                        <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                          {cita.tecnico_documento} - {cita.tecnico_email}
+                        </td>
+                        <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                          {cita.diagnostico || "-"}
+                        </td>
+                        <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                          {cita.estado || "-"}
+                        </td>
+                        <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                          {cita.direccion_servicio}
+                        </td>
+                        <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                          {cita.garantia}
+                        </td>
+                        <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                          {cita.descripcion_problema}
+                        </td>
+                        <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                          {cita.solucion || "-"}
+                        </td>
+                        <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                          {cita.observacion || "-"}
+                        </td>
+                        <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                          {cita.imagen_evidencia ? (
+                            <img
+                              src={cita?.imagen_evidencia}
+                              className="w-[4rem] h-[4rem]"
+                            />
+                          ) : (
+                            "-"
+                          )}
+                        </td>
+
+                        <td className="flex flex-col h-[7rem] w-full gap-2 px-2 py-5 border-b border-gray-200 bg-white text-sm">
+                          {compareFechaCitas(cita) ? (
+                            <>
+                              <button
+                                className="flex items-center "
+                                onClick={() => {
+                                  alert(
+                                    "No puede editar ni cancelar esta cita. Ya esta en proceso."
+                                  );
+                                }}
+                              >
+                                <IconoProceso />
+                                En proceso
+                              </button>
+
+                              <button
+                                className="flex items-center"
+                                onClick={() => {
+                                  handleOpenDataModal(cita);
+                                  setModal3(!open3);
+                                }}
+                              >
+                                <IconoReagendar /> reagendar
+                              </button>
+                            </>
+                          ) : (
+                            <button
+                              className="flex items-center"
+                              onClick={() => {
+                                handleOpenDataModal(cita);
+                                setModal3(!open3);
+                              }}
+                            >
+                              <IconoReagendar /> reagendar
+                            </button>
+                          )}
+                        </td>
+                      </tr>
+                    </>
+                  ))}
+                </tbody>
+              </table>
+              {/** REAGENDAR */}
+              <Modal isOpen={open3} onClose={() => setModal3(!open3)}>
+                <form
+                  className="h-full w-full p-[1rem]"
+                  onSubmit={handleSubmitEdit}
+                >
+                  <div className="col-span-full">
+                    <label
+                      htmlFor="horaCita"
+                      className="block text-gray-700 text-sm font-bold mb-2"
+                    >
+                      Fecha de cita (opcional)
+                    </label>
+
+                    <input
+                      type="date"
+                      id="fechaCita"
+                      className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                      value={selectedDate}
+                      onChange={(e) => setSelectedDate(e.target.value)}
+                      min={minDate}
+                    />
+                  </div>
+                  <div className="col-span-full">
+                    <label
+                      htmlFor="horaCita"
+                      className="block text-gray-700 text-sm font-bold mb-2"
+                    >
+                      Hora de cita (opcional)
+                    </label>
+                    <div className="flex flex-col">
+                      <button
+                        className="flex w-full gap-2 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+                        type="button"
+                        onClick={handleShowSelectTimer}
+                      >
+                        Seleccionar{" "}
+                        {selectedTime ? (
+                          <span className=" text-gray-700">
+                            Hora: {selectedTime}
+                          </span>
+                        ) : (
+                          <span className=" text-gray-700">Hora: ...</span>
+                        )}
+                      </button>
+                      {showTime && (
+                        <DateTimer
+                          citas={citas}
+                          fechaInicioFormateada={fecha}
+                          handleShowSelectTimer={handleShowSelectTimer}
+                          selectedTime={selectedTime}
+                          setSelectedTime={setSelectedTime}
+                        />
+                      )}
+                    </div>
+                  </div>
+                  <button
+                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                    type="submit"
+                  >
+                    Enviar
+                  </button>
+                </form>
+              </Modal>
+            </div>
+          )}
         </div>
       </div>
     </div>
